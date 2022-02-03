d386a0a305096794d026057a738c0402bd99364b
yassine_desktop <elmeftouhi@extensionmedia.ma>
2022-02-03 22:43:18 +0000

    add notifications object and show hide notif container

--- src/components/topbar/Notifications/Notifications.jsx
+++ src/components/topbar/Notifications/Notifications.jsx
@@ -3,17 +3,40 @@ import { ChevronUp } from 'react-feather';
 import NotificationsItem from './NotificationsItem';
 
 const Notifications = () => {
+  const notifs = [
+    {
+      "id"    :   1,
+      "text"  :   "Vous avez attent le budget",
+      "date"  :   "2022-02-02"
+    },
+    {
+      "id"    :   2,
+      "text"  :   "Une nouvelle connection",
+      "date"  :   "2022-02-01"
+    },
+    {
+      "id"    :   3,
+      "text"  :   "Voua avez modifier",
+      "date"  :   "2022-02-01"
+    },
+    {
+      "id"    :   4,
+      "text"  :   "Une simple tonification",
+      "date"  :   "2022-01-31"
+    }
+  ]
   return (
     <div className='absolute top-10 right-1 w-80 md:w-96 bg-gray-800 text-white shadow-lg'>
         <div className="relative border-t-2 border-green-700">
             <ChevronUp className='absolute -top-5 right-0 text-green-700' />
             <ul>
-                <NotificationsItem />
-                <NotificationsItem />
-                <NotificationsItem />
-                <NotificationsItem />
-                <NotificationsItem />
-                <NotificationsItem />
+              {
+                notifs.map(
+                  (item) =>(
+                    <NotificationsItem item={item} key={item.id} />
+                  )
+                )
+              }                
             </ul>
             <div className="text-center py-4 text-xs">
                 Voir tous les notifications
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,9 +1,10 @@
 import {React, useState} from 'react';
-import { Menu, Bell, Search } from 'react-feather';
+import { Menu, Bell, Search, X } from 'react-feather';
 import SearchBar from './Search/SearchBar'
 import Notifications from './Notifications/Notifications';
 function TopBar() {
   const [showSearch, setShowSearch] = useState(false)
+  const [showNotifications, setShowNotifications] = useState(false)
   
   return (
     <div className='w-full shadow bg-white relative'>
@@ -19,9 +20,14 @@ function TopBar() {
                 <Search />
             </div>
             <div className="relative">
-                <Bell className="cursor-pointer transition-all p-1 h-8 w-8 hover:bg-green-700 hover:text-white rounded-full" />
-                <div className="px-[4px] ml-4 absolute -top-1  bg-red-500 text-white rounded-full text-xs text-center">1</div>
-                <Notifications />
+                { !showNotifications && <div className="">
+                <Bell onClick={() => setShowNotifications(!showNotifications)} className="cursor-pointer transition-all p-1 h-8 w-8 hover:bg-green-700 hover:text-white rounded-full" />
+                  <div className="px-[4px] ml-4 absolute -top-1  bg-red-500 text-white rounded-full text-xs text-center">1</div>
+                </div>}
+                { showNotifications && <div onClick={() => setShowNotifications(!showNotifications)} className="cursor-pointer hover:text-red-500 p-1 transition-all">
+                  <X />
+                </div>}
+                { showNotifications && <Notifications showNotifications={showNotifications} setShowNotifications={setShowNotifications} />}
             </div>
           </div>
         </div> 
